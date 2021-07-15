77c33adba23745c45786d4bb1aae687f51d2c5b5
Mahadi001 <sadat.himel@gmail.com>
2021-07-16 04:30:08 +0600

    character program update

---
+++ ch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    /*
+    int x,y,z;
+
+    z = x+y;
+
+    printf("Enter the number of x and enter the number of y\n");
+    scanf("%d %d", &x, &y);
+    z = x + y;
+    printf("The value of %d + %d equal %d\n",x,y,z); */
+    char ch;
+
+    printf("Enter the char:");
+    scanf("%c", &ch);
+    printf("The character you entered %c",ch);
+
+    return 0;
+}
