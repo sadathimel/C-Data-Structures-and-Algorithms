b6d7e64a14f42869e7ccfd139e2e744ccd5a39ae
Mahadi001 <sadat.himel@gmail.com>
2021-07-16 17:04:26 +0600

    float and double

---
+++ double.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    double d;
+    scanf("%lf", &d);
+    printf("%.2lf", d);
+    return 0;
+}
---
+++ float.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+
+int main(){
+    float f;
+    printf("Enter the float number of :");
+    scanf("%f", &f);
+    printf("%f", f);
+    return 0;
+}
