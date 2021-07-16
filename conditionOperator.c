bd768f1da081e022932c821cbf9d8dbe0836219d
Mahadi001 <sadat.himel@gmail.com>
2021-07-17 01:28:31 +0600

    conditional program

--- conditionOperator.c
+++ conditionOperator.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
 
+int main(){
+    char ch;
+    scanf("%c",&ch);
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+        printf("This is a vawel");
+    }else{
+        printf("This is a concunent");
+    }
+    return 0;
+}
